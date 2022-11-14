a7f4c6a0fff908d587989c44fdedf64c30aeebd1
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-11-14 09:57:16 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @returns the factorial of the num parameter passed in
+ */
+function FirstFactorial(num) {
+  let factorial = 1
+  for (let i = 1; i <= num; i++) factorial *= i
+  return factorial
+}
