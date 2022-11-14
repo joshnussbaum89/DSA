9c33a443366ed6d25dfc212eb79d9b4eca2e5c66
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-11-14 09:58:51 -0600

    add README

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @param {number} num
  * @returns the factorial of the num parameter passed in
  */
-function FirstFactorial(num) {
+function firstFactorial(num) {
   let factorial = 1
   for (let i = 1; i <= num; i++) factorial *= i
   return factorial
