43eed5b08dc91f193026561f1b087fb546606308
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-11-15 09:43:11 -0600

    find intersection

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function firstFactorial(num) {
   for (let i = 1; i <= num; i++) factorial *= i
   return factorial
 }
+
+/**
+ *
+ * @param {object} strArr - Array of two strings containing comma separated string number values ex/ ['1, 2, 3, 4', '5, 6, 7, 8']
+ * @returns A string of the numbers that occur in both strings
+ */
+function findIntersection(strArr) {
+  let arrayOne = strArr[0].split(', ')
+  let arrayTwo = strArr[1].split(', ')
+
+  let intersectedNumbers = arrayTwo
+    .filter((num) => arrayOne.includes(num))
+    .sort((a, b) => parseInt(a) - parseInt(b))
+    .join(',')
+
+  return intersectedNumbers ? intersectedNumbers : false
+}
