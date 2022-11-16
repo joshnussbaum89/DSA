ce4571bd798ba9f9ec407ea5c2ab5bff30fb42ae
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-11-16 11:32:57 -0600

    longest word

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function firstFactorial(num) {
 }
 
 /**
- *
  * @param {object} strArr - Array of two strings containing comma separated string number values ex/ ['1, 2, 3, 4', '5, 6, 7, 8']
  * @returns A string of the numbers that occur in both strings
  */
@@ -23,4 +22,28 @@ function findIntersection(strArr) {
     .join(',')
 
   return intersectedNumbers ? intersectedNumbers : false
+}
+
+/**
+ * Determine the longest word from a parameter containing a string of words
+ * @param {string} sen
+ * @returns longest word in string provided
+ */
+function longestWord(sen) {
+  // Sort and remove special chars
+  const sorted = sen
+    .split(' ')
+    .map((str) => str.replace(/[^a-zA-Z0-9]/g, ''))
+    .sort((a, b) => a.length - b.length)
+
+  // IF only one word was submitted
+  if (sorted.length === 1) return sorted[0]
+
+  // IF another word length matches > return the first one
+  const longest =
+    sorted[sorted.length - 1].length === sorted[sorted.length - 2].length
+      ? sorted[sorted.length - 2]
+      : sorted[sorted.length - 1]
+
+  return longest
 }
